913322e5a9d1db3e1ab8b4940184f45a16fd018f
gerson aguiar <gersoncafilho@gmail.com>
2024-07-04 21:42:32 -0300

    test dialog com herbert

--- src/pages/MyEvents.js
+++ src/pages/MyEvents.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Button, TableHead } from '@mui/material';
+import { Box, Button, TableHead, Typography } from '@mui/material';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -11,7 +11,10 @@ import DialogContent from '@mui/material/DialogContent';
 import TableRow from '@mui/material/TableRow';
 import FormEventComponent from '../components/FormEvent';
 import useEvents from '../service/realtime.database';
+import Soma from './TesteDialog';
 
+// MOCK
+// const MOCK_EVENTOS_RESPONSE
 const visibleRows = [
   {
     id: 1,
@@ -32,7 +35,15 @@ const visibleRows = [
 ];
 
 const MyEvents = () => {
-  const handleClick = (event, id) => {};
+  // const valor = Soma(5, 6);
+  // console.log(valor);
+
+  const [showSoma1, setShowForm1] = useState(false);
+
+  const handleClick = (event, id) => {
+    setShowForm1(!showSoma1);
+  };
+
   const [showForm, setShowForm] = useState(false);
 
   const events = useEvents();
@@ -44,79 +55,89 @@ const MyEvents = () => {
   const combinedRows = [...events, ...visibleRows];
 
   return (
-    <Box mt={10}>
-      <Button
-        sx={{ mb: 4 }}
-        variant="contained"
-        onClick={() => setShowForm(true)}
-      >
-        Criar Evento
-      </Button>
+    <>
+      {/* JSX */}
+      {showSoma1 && (
+        <>
+          <Dialog open>Alo</Dialog>
+          <Soma a={4} b={9} />
+        </>
+      )}
 
-      <Dialog open={showForm} onClose={() => setShowForm(false)}>
-        <DialogContent>
-          <FormEventComponent handleClose={() => setShowForm(false)} />
-        </DialogContent>
-      </Dialog>
+      <Box mt={10}>
+        <Button
+          sx={{ mb: 4 }}
+          variant="contained"
+          onClick={() => setShowForm(true)}
+        >
+          Criar Evento
+        </Button>
 
-      <TableContainer>
-        <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle">
-          <TableHead>
-            <TableRow
-              hover
-              role="checkbox"
-              tabIndex={-1}
-              sx={{ cursor: 'pointer' }}
-            >
-              <TableCell align="left">Ativo</TableCell>
+        <Dialog open={showForm} onClose={() => setShowForm(false)}>
+          <DialogContent>
+            <FormEventComponent handleClose={() => setShowForm(false)} />
+          </DialogContent>
+        </Dialog>
 
-              <TableCell component="th" scope="row" padding="none">
-                Nome
-              </TableCell>
-              <TableCell align="right">Data</TableCell>
-              <TableCell align="right">Valor</TableCell>
-              <TableCell align="right">Capacidade</TableCell>
-              <TableCell align="right">Vendidos</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {combinedRows.map((row, index) => {
-              const isItemSelected = true;
-              const labelId = `enhanced-table-checkbox-${index}`;
+        <TableContainer>
+          <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle">
+            <TableHead>
+              <TableRow
+                hover
+                role="checkbox"
+                tabIndex={-1}
+                sx={{ cursor: 'pointer' }}
+              >
+                <TableCell align="left">Ativo</TableCell>
 
-              return (
-                <TableRow
-                  hover
-                  onClick={(event) => handleClick(event, row.id)}
-                  role="checkbox"
-                  aria-checked={isItemSelected}
-                  tabIndex={-1}
-                  key={row.id}
-                  selected={isItemSelected}
-                  sx={{ cursor: 'pointer' }}
-                >
-                  <TableCell padding="checkbox" align="center">
-                    <Switch size="small" defaultChecked />
-                  </TableCell>
-                  <TableCell
-                    component="th"
-                    id={labelId}
-                    scope="row"
-                    padding="none"
+                <TableCell component="th" scope="row" padding="none">
+                  Nome
+                </TableCell>
+                <TableCell align="right">Data</TableCell>
+                <TableCell align="right">Valor</TableCell>
+                <TableCell align="right">Capacidade</TableCell>
+                <TableCell align="right">Vendidos</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {combinedRows.map((row, index) => {
+                const isItemSelected = true;
+                const labelId = `enhanced-table-checkbox-${index}`;
+
+                return (
+                  <TableRow
+                    hover
+                    onClick={(event) => handleClick(event, row.id)}
+                    role="checkbox"
+                    aria-checked={isItemSelected}
+                    tabIndex={-1}
+                    key={row.id}
+                    selected={isItemSelected}
+                    sx={{ cursor: 'pointer' }}
                   >
-                    {row.title}
-                  </TableCell>
-                  <TableCell align="right">{row.date}</TableCell>
-                  <TableCell align="right">{row.price}</TableCell>
-                  <TableCell align="right">{row.capacity}</TableCell>
-                  <TableCell align="right">{row.sold}</TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Box>
+                    <TableCell padding="checkbox" align="center">
+                      <Switch size="small" defaultChecked />
+                    </TableCell>
+                    <TableCell
+                      component="th"
+                      id={labelId}
+                      scope="row"
+                      padding="none"
+                    >
+                      {row.title}
+                    </TableCell>
+                    <TableCell align="right">{row.date}</TableCell>
+                    <TableCell align="right">{row.price}</TableCell>
+                    <TableCell align="right">{row.capacity}</TableCell>
+                    <TableCell align="right">{row.sold}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </>
   );
 };
 
---
+++ src/pages/TesteDialog.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Soma = (pros) => {
+  const { justificativa } = pros;
+  //   return <div>TesteDialog</div>;
+  return <h1>{justificativa}</h1>;
+};
+
+export default Soma;
